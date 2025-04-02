fa8a2481445f05a285e0ac79903d79fadcbfc457
Yaryna Pechenenko <pechenenko.pn@ucu.edu.ua>
2025-04-03 00:20:23 +0300

    task2

--- tasks.py
+++ tasks.py
@@ -1,3 +1,8 @@
+class Node:
+    def __init__(self, data, next=None): 
+        self.data = data
+        self.next = next
+        
 def stringify(node):
     res = ''
     while node:
@@ -5,3 +10,17 @@ def stringify(node):
         node = node.next
     res += 'None'
     return res
+
+
+def linked_list_from_string(s):
+    values = [int(x) for x in s.split(" -> ") if x != "None"]
+    head = None
+    current = None
+    for value in values:
+        if head is None:
+            head = Node(value)
+            current = head
+        else:
+            current.next = Node(value)
+            current = current.next
+    return head
