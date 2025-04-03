eb43fa9a23e1d6b053a711275a20a3066c0ed19b
Yaryna Pechenenko <pechenenko.pn@ucu.edu.ua>
2025-04-03 22:16:40 +0300

    task 6

--- tasks.py
+++ tasks.py
@@ -59,3 +59,16 @@ def move_node(source, dest):
     dest = push(dest, source.data)
     source = source.next
     return Context(source, dest)
+
+
+def sorted_insert(head, data):
+    new_node = Node(data)
+    if head is None or data < head.data:
+        new_node.next = head
+        return new_node
+    current = head
+    while current.next is not None and current.next.data < data:
+        current = current.next
+    new_node.next = current.next
+    current.next = new_node
+    return head
