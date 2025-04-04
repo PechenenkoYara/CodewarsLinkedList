c7a7782c6b16ac136d93da7af857cb2fd6db9fcd
Yaryna Pechenenko <pechenenko.pn@ucu.edu.ua>
2025-04-04 18:36:58 +0300

    task9

--- tasks.py
+++ tasks.py
@@ -113,3 +113,33 @@ def swap_pairs(head):
         head = first.next
     return start.next
 
+def alternating_split(head):
+    if head is None:
+        raise ValueError("Input list cannot be None")
+    if head.next is None:
+        raise ValueError("Input list cannot be None")
+
+    a_head = a_tail = None
+    b_head = b_tail = None
+    turn = True 
+
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = None 
+        if turn:
+            if a_head is None:
+                a_head = a_tail = current
+            else:
+                a_tail.next = current
+                a_tail = current
+        else:
+            if b_head is None:
+                b_head = b_tail = current
+            else:
+                b_tail.next = current
+                b_tail = current
+        turn = not turn
+        current = next_node
+    return Context(a_head, b_head)
