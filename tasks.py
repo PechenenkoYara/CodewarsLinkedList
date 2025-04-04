eb9cf811f2d6dfb17b98b8260c317d55d84132c7
Yaryna Pechenenko <pechenenko.pn@ucu.edu.ua>
2025-04-04 18:11:33 +0300

    task9

--- tasks.py
+++ tasks.py
@@ -97,3 +97,19 @@ def remove_duplicates(head):
     return head
 
 
+def swap_pairs(head):
+    if head is None:
+        return head
+    start = Node(0)
+    start.next = head
+    current = start
+    while head and head.next:
+        first = head
+        second = head.next
+        current.next = second
+        first.next = second.next
+        second.next = first
+        current = first
+        head = first.next
+    return start.next
+
